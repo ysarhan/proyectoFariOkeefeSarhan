47dd66201aed0c65461dd7b116a1c4c0f5947a16
abrilfari <afarinaccia@udesa.edu.ar>
2025-11-13 16:25:10 -0300

    Agregué el index.js y el login.js

---
+++ js/index.js
@@ -0,0 +1,42 @@
+let url = "https://dummyjson.com/products/category/groceries?limit=10"
+fetch(url)
+    .then(function (res) {
+        return res.json();
+    })
+
+    .then(function (data) {
+        let results = data.products;
+        let info = document.querySelector(".nuestrosprod")
+        let productos = "";
+
+        for (let i = 0; i < results.length; i++) {
+            productos += `
+                <article class="imagen">
+                    <img src= ${results[i].images}></img>
+                    <h3> ${results[i].title}</h3>
+                    <p class="texto">${results[i].description}</p>
+                    <p class="precio">${results[i].price} </p>
+                    <a href="./product.html" class="ver_detalle">Ver detalle</a>
+                </article>
+            `
+        }
+        info.innerHTML = productos
+
+        let info2 = document.querySelector(".vendidos")
+        let vendidos = ""
+        for (let i = 0; i < results.length; i++) {
+            vendidos += `
+                <article class="imagen">
+                    <img src= ${results[i].images}></img>
+                    <h3> ${results[i].title}</h3>
+                    <p class="texto">${results[i].description}</p>
+                    <p class="precio">${results[i].price} </p>
+                    <a href="./product.html" class="ver_detalle">Ver detalle</a>
+                </article>
+            `
+        }
+        info2.innerHTML = vendidos
+    })
+    .catch(function (error) {
+        console.log("Error:" + error);
+    })
