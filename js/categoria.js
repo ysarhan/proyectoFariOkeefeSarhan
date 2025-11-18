80b39ecb5c434570cc86c065186c513b63a994ee
ysarhan <ysarhan@udesa.edu.ar>
2025-11-18 19:23:20 -0300

    agregue sidebar dinamico a categorias

--- js/categoria.js
+++ js/categoria.js
@@ -1,4 +1,37 @@
-let url = "https://dummyjson.com/products/category/beauty"
+let url2 = "https://dummyjson.com/products/category-list"
+fetch(url2)
+    .then(function(response) {
+        return response.json();
+    })
+    .then (function (data) {
+        let results = data
+        let lista = document.querySelector(".categoria_sidebar");
+        let template = "";
+
+
+        for (let i = 0; i<results.length; i++) {
+            template += `
+                <li class="cat">
+                   <a href="./category.html?category=${results[i]}" class="categorias_">
+                        ${results[i]}
+                </li>
+            `;
+            }
+        lista.innerHTML = template;    
+    })
+    .catch(function (error) {
+        console.log("Error cargando categorias: " + error);
+    })
+
+
+
+
+
+let url = "https://dummyjson.com/products/categories"
+let categoria = parametros.get("cat"); 
+let titulo = document.querySelector("h2");
+titulo.textContent = "Categoria | " + categoria; 
+
 fetch(url)
     .then(function(response) {
         return response.json();
@@ -11,7 +44,7 @@ fetch(url)
             productos += `
                 <article class = "imagen">
                     <img src= ${results[i].images}></img>
-                    <p><a href="./product.html" class="ver_detalle">Ver detalle</a></p>
+                    <p><a href="./product.html?id=${results[i].id}" class="ver_detalle">Ver detalle</a></p>
                 </article>
             `
         }
@@ -20,3 +53,4 @@ fetch(url)
     .catch (function(erorr) {
         console.log ("error:" + error)
     })
+
