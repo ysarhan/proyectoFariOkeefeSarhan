eb5094ea74322e389e1d2336aef1cfa2d905cc99
ysarhan <ysarhan@udesa.edu.ar>
2025-11-19 16:31:45 -0300

    edite js de busqueda

--- js/busqueda.js
+++ js/busqueda.js
@@ -39,3 +39,44 @@ formulario.addEventListener("submit", function(event){
          this.submit()
     } 
 })
+
+
+let parametros = new URLSearchParams(location.search);
+let termino = parametros.get("search");
+let titulo = document.querySelector(".titulo_busqueda");
+let contenedor = document.querySelector(".contenedor_resultados");
+let noResultados = document.querySelector(".no_hay_resultados");
+
+titulo.innerText = `${termino}`;
+let url = "https://dummyjson.com/products/search?q=" + termino 
+fetch(url) 
+    .then(function(response) {
+        return response.json();
+    })
+    .then (function(data) {
+        let results = data.products ;
+        
+        if (results.length == 0){
+            noResultados.style.display = "block";
+            contenedor.innerHTML = "";
+        } else {
+            noResultados.style.display = "none";
+            let html = "";
+            for (let i = 0; i < results.length; i++) {
+                html += ` 
+                    <article class="imagen">
+                        <img src="${results[i].images[0]}">
+                        <p>${results[i].title}</p>
+                        <a href="./product.html?id=${results[i].id}" class="ver_detalle">Ver detalle</a>
+                    </article>
+                `;
+            }
+            contenedor.innerHTML = html; 
+        }
+    })
+
+    .catch(function(error){
+        console.log("Error: " + error);
+    }); 
+
+    
